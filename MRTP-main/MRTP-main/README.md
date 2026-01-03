2ba84a24e795cbdefed824bacf6db6e0532f5a61
nguykevu-gif <nguykevu@gmail.com>
2026-01-03 16:31:14 +0700

    Add files via upload

---
+++ MRTP-main/MRTP-main/README.md
@@ -0,0 +1,21 @@
+⚠️**Attention**
+
+Check the video on https://youtube.com/@SmoothDudeU-U to make sure you download the texture pack right.
+
+⭐ **Helps**
+
+I really wanna say thanks to heythereu, which helped me a lot in this journey. Without him this texture pack would have required x10 time i used
+
+💡 **Credits**
+
+Ores outline: SmoothDude6969
+
+Enchantment glint: Minecraft Rainbow Glint texture pack
+
+Custom background: MinecraftImages
+
+If you wanna DM me for issues or anything send to: 
+
+@smoothdudee / SmoothDude
+
+-SmoothDude
---
+++ MRTP-main/MRTP-main/background.js
@@ -0,0 +1,74 @@
+const URL_MAP = {
+  "||miniblox.*textures/items/infernium.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/50aeb2db-9a3e-43ed-8272-e8788c27996b.png",
+  "||miniblox.*textures/spritesheet.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/refs/heads/main/spritesheet-t.png",
+  "||miniblox.*assets/default-DKNlYibk.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/Screenshot_2.png",
+  "||miniblox.*assets/miniblox-Dj36hMhG": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/miniblox_text_no_white.png",
+  "||miniblox.*assets/Miniblox-Regular-Lz-C8nWS.otf": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/assets/Miniblox-Regular-Lz-C8nWS.otf",
+  "||miniblox.*textures/entity/chicken/chicken.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/tavuk.png",
+  "||miniblox.*textures/entity/cow/cow.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/main/rainbow-cow.png",
+  "||miniblox.*textures/entity/creeper/creeper.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/fbi-creeper.png",
+  "||miniblox.*textures/entity/pig/pig.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/fbi-agent-plus-glasses-pig.png", 
+  "||miniblox.*textures/entity/sheep/sheep_fur.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/sheep_fur.png",
+  "||miniblox.*textures/entity/skeleton/skeleton.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/skeleton.png",
+  "||miniblox.*textures/entity/slime/slime.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/slime.png",
+  "||miniblox.*textures/entity/spider/spider.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/spider.png",
+  "||miniblox.*textures/entity/zombie/zombie.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/zombie.png",
+  "||miniblox.*textures/entity/ghost/ghost.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/ghost.png",
+  "||miniblox.*textures/entity/zombie_cowman/zombie_cowman.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/zombie_cowman.png",
+  "||miniblox.*textures/entity/snowman/snowman.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/snowman.png",
+  "||miniblox.*textures/entity/arrow.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/arrow.png",
+  "||miniblox.*textures/entity/boat/acacia.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/acacia.png",
+  "||miniblox.*textures/entity/boat/birch.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/birch.png",
+  "||miniblox.*textures/entity/boat/dark_oak.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/dark_oak.png",
+  "||miniblox.*textures/entity/boat/jungle.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/jungle.png",
+  "||miniblox.*textures/entity/boat/oak.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/oak.png",
+  "||miniblox.*textures/entity/boat/spruce.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/entity/spruce.png",
+  "||miniblox.*textures/models/armor/chainmail_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/chainmail_layer_1.png",
+  "||miniblox.*textures/models/armor/chainmail_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/chainmail_layer_2.png",
+  "||miniblox.*textures/models/armor/diamond_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/diamond_layer_1.png",
+  "||miniblox.*textures/models/armor/diamond_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/diamond_layer_2.png",
+  "||miniblox.*textures/models/armor/emerald_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/emerald_layer_1.png",
+  "||miniblox.*textures/models/armor/emerald_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/emerald_layer_2.png",
+  "||miniblox.*textures/models/armor/infernium_layer_1.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/netheritelayer1.png",
+  "||miniblox.*textures/models/armor/infernium_layer_2.png": "https://raw.githubusercontent.com/nguykevu-gif/B-7-m-u-/refs/heads/main/netheritelayer2.png",
+  "||miniblox.*textures/models/armor/gold_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/gold_layer_1.png",
+  "||miniblox.*textures/models/armor/gold_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/gold_layer_2.png",
+  "||miniblox.*textures/models/armor/iron_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/iron_layer_1.png",
+  "||miniblox.*textures/models/armor/iron_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/iron_layer_2.png",
+  "||miniblox.*textures/models/armor/leather_layer_1.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/leather_layer_1.png",
+  "||miniblox.*textures/models/armor/leather_layer_2.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/main/armor/leather_layer_2.png",
+  "||miniblox.*textures/misc/enchanted_item_glint.png": "https://raw.githubusercontent.com/SmoothDude6969/MRTP/refs/heads/main/DEBUG/enchanted_item_glint.png"
+};
+
+let rules = [];
+let idx = 1;
+
+for (const [src, dst] of Object.entries(URL_MAP)) {
+  rules.push({
+    "id": idx++,
+    "action": {
+      "type": "redirect",
+      "redirect": { "url": dst }
+    },
+    "condition": {
+      "urlFilter": src,
+      "resourceTypes": src.endsWith(".otf") ? ["font"] : ["image"]
+    }
+  });
+}
+
+chrome.declarativeNetRequest.updateDynamicRules(
+  {
+    addRules: rules,
+    removeRuleIds: rules.map(rule => rule.id)
+  },
+  () => {
+    if (chrome.runtime.lastError) {
+      console.error("Error updating:", chrome.runtime.lastError);
+    } else {
+      console.log("Rules updated");
+    }
+  }
+);
+
+
